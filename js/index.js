573d76567c81125f452219b0333d73a2f30369f2
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-01-25 19:52:21 -0300

    Implementacion nueva funcion

--- js/index.js
+++ js/index.js
@@ -20,3 +20,25 @@ let componentes = parseInt(prompt("Ingrese la cantidad de componentes que quiere
 for(i=0; i <= componentes; i++){
     console.log("Componentes sumados al carrito: " + i)
 }
+
+function compra(producto, cantidad){
+    if(producto === "cpu"){
+        precio = 150000;
+        precioTotal = precio * cantidad;
+        console.log("Usted debe pagar: $" + precioTotal);
+    } else if(producto === "motherboard"){
+        precio = 99000;
+        precioTotal = precio * cantidad;
+        console.log("Usted debe pagar: $" + precioTotal);
+    } else if(producto === "gpu"){
+        precio = 249000;
+        precioTotal = precio * cantidad;
+        console.log("Usted debe pagar: $" + precioTotal);
+    } else {
+        console.log("Usted eligio el producto incorrecto");
+    }
+}
+
+let producto = prompt("Elija un producto (CPU, Motherboard, GPU)").toLowerCase();
+let cantidad = parseInt(prompt("Ingrese la cantidad que quiere comprar"));
+compra(producto, cantidad)
