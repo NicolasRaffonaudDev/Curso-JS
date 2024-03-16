34d8f48823ba00b167d9733c51a0e081798d3f9b
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-03-16 17:42:14 -0300

    pequeño cambio desde el trabajo

--- js/index.js
+++ js/index.js
@@ -327,27 +327,5 @@ console.log(EJEMPLO);
 const EJEMPLO_PASADO = JSON.parse(EJEMPLO);
 console.log(EJEMPLO_PASADO);
 
-// PRACTICO EL LOGIN
+// LIBRERIAS
 
-const LOGEANDO = document.getElementById('loginForm').addEventListener('submit', function(e){
-    e.preventDefault();
-
-    let usuario = document.getElementById('user').value;
-    let password = document.getElementById('password').value;
-
-    if(usuario === 'nicolas' && password === 'atlantida'){
-        localStorage.setItem('logeoCorrecto', true);
-
-        window.location.href = 'login.html';
-    }else {
-        document.getElementById('messageLogin').innerText = 'Datos Incorrectos';
-    }
-})
-
-// COMPROBACION
-
-window.addEventListener('load', function(){
-    if(localStorage.getItem('logeoCorrecto')){
-        this.window.location.href = 'login.html';
-    }
-})
