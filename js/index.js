197b9e871ac8f5d43a19911a03f42728b73c0f75
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2024-03-20 12:35:36 -0300

    metiendo imagen al catalogo

--- js/index.js
+++ js/index.js
@@ -2,22 +2,22 @@
 
 let componentes = 1; //parseInt(prompt("Ingrese la cantidad de componentes que quiere comprar para su computadora"));
 
-for(i=1; i <= componentes; i++){
+for (i = 1; i <= componentes; i++) {
     console.log("Componentes sumados al carrito: " + i)
-} 
+}
 
 /* funcion para comprar componentes */
 
-function compra(producto, cantidad){
-    if(producto === "cpu"){
+function compra(producto, cantidad) {
+    if (producto === "cpu") {
         precio = 150000;
         precioTotal = precio * cantidad;
         console.log("Usted debe pagar: $" + precioTotal);
-    } else if(producto === "motherboard"){
+    } else if (producto === "motherboard") {
         precio = 99000;
         precioTotal = precio * cantidad;
         console.log("Usted debe pagar: $" + precioTotal);
-    } else if(producto === "gpu"){
+    } else if (producto === "gpu") {
         precio = 249000;
         precioTotal = precio * cantidad;
         console.log("Usted debe pagar: $" + precioTotal);
@@ -28,14 +28,14 @@ function compra(producto, cantidad){
 
 let producto = 0; //prompt("Elija un producto (CPU, Motherboard, GPU)").toLowerCase();
 let cantidad = 0; //parseInt(prompt("Ingrese la cantidad que quiere comprar"));
-compra(producto, cantidad) 
+compra(producto, cantidad)
 
 /* SEGUNDA PRE-ENTREGA */
 
 
 /* AGREGANDO OBJETOS */
 
-function ComponentesPc(lista, motherboard, cpu, gpu, ram, disco, fuente, gabinete, stock){
+function ComponentesPc(lista, motherboard, cpu, gpu, ram, disco, fuente, gabinete, stock) {
     /* this.informe = function(){console.log("Hola, soy la PC MASTER RAZE y tengo estos componentes: ")}; */
     this.lista = lista;
     this.motherboard = motherboard;
@@ -53,11 +53,11 @@ const PC_DOS = new ComponentesPc(2, "Asus-Prime", "Intel-I5", "1060TI", "Corsair
 const PC_TRES = new ComponentesPc(3, "Asus-Prime", "Ryzen-3-3600", "AMD-RX-5500", "Corsair2x8", "SSD256GB", "Seasonic", "Corsair", false)
 const PC_CUATRO = new ComponentesPc(4, "Asus-Prime", "Intel-I7", "RTX-2080", "Corsair2x16", "SSD512GB", "Seasonic", "Corsair", false)
 const PC_CINCO = new ComponentesPc(5, "Asus-Prime", "Ryzen-7-7800x", "RX-6600", "Corsair2x16", "SSD512GB", "Seasonic", "Corsair", false)
-const PC_SEIS = new ComponentesPc(6, "Asus-Prime", "Intel-I9", "RTX-3090","Corsair2x32", "SSD1B", "Seasonic", "Corsair", true)
+const PC_SEIS = new ComponentesPc(6, "Asus-Prime", "Intel-I9", "RTX-3090", "Corsair2x32", "SSD1B", "Seasonic", "Corsair", true)
 const PC_SIETE = new ComponentesPc(7, "Asus-Prime", "Ryzen-9-9700x", "AMD-RX-7900", "Corsair2x32", "SSD1TB", "Seasonic", "Corsair", true)
 
-function compraPc(numero){
-    switch(numero){
+function compraPc(numero) {
+    switch (numero) {
         case 1:
             return PC_UNO;
             break;
@@ -129,30 +129,30 @@ console.log("En stock = ", PC_STOCK)
 
 const btn = document.getElementById("btn");
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     alert('Se esta ejecutando el evento')
 })
 
 const cambiarColor = document.getElementById("cambiarColor");
 
-cambiarColor.addEventListener('click', function(){
+cambiarColor.addEventListener('click', function () {
     document.body.style.background = 'none';
 })
 
 const btnNodo = document.getElementById("btnNodo");
 
-btnNodo.addEventListener('click', function(){
+btnNodo.addEventListener('click', function () {
     alert('se esta ejecuntando el evento a traves del nodo')
 })
 
 let menuBoton = document.getElementById("menuBoton");
 let menu = document.getElementById("menu");
 
-menuBoton.addEventListener('click', function(){
-    menu.style.display = (menu.style.display === 'block')? 'none' : 'block';
+menuBoton.addEventListener('click', function () {
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 })
 
-function jsEnHTML(){
+function jsEnHTML() {
     alert("Se esta ejecutando por HTML");
 }
 
@@ -163,8 +163,8 @@ let emailInput = document.getElementById("email");
 let mensajeInput = document.getElementById("mensaje");
 let emailError = document.getElementById("emailError");
 
-emailInput.addEventListener('input', function(){
-    if(!this.validity.valid){
+emailInput.addEventListener('input', function () {
+    if (!this.validity.valid) {
         emailError.style.display = 'block';
     } else {
         emailError.style.display = 'none'
@@ -173,34 +173,34 @@ emailInput.addEventListener('input', function(){
 
 let formDeRegistro = document.getElementById("formDeRegistro");
 
-formDeRegistro.addEventListener("submit", function(event){
-    if(!nombreInput.validity.valid || !emailInput.validity.valid || !mensajeInput.validity.valid){
-        if(!nombreInput.validity.valid){
+formDeRegistro.addEventListener("submit", function (event) {
+    if (!nombreInput.validity.valid || !emailInput.validity.valid || !mensajeInput.validity.valid) {
+        if (!nombreInput.validity.valid) {
             alert("Hay un error al ingresar su nombre");
         }
-        if(!emailInput.validity.valid){
+        if (!emailInput.validity.valid) {
             emailError.style.display = "block";
             alert("Hay un error al ingresar su Email");
         }
-        if(!mensajeInput.validity.valid){
+        if (!mensajeInput.validity.valid) {
             alert("Escribanos todo lo que necesite, gracias");
         }
         event.preventDefault();
-    } else{
+    } else {
         alert("Gracias por dejarnos tu mensaje, te contactamos a la brevedad");
     }
-}); 
+});
 
 
 //CARRITO DE COMPRAS PARA NUESTRO PROYECTO
 
 const PRODUCTOS = [
-    {id:1, nombre:"ryzen-3-3200g", precio:99000,},
-    {id:2, nombre:"ryzen-5-3600", precio:149000},
-    {id:3, nombre:"intelCore-i5-10400F", precio:136900},
-    {id:4, nombre:"intelCore-i7-12700F", precio:381900},
-    {id:5, nombre:"ryzen-7-7700", precio:379000},
-    {id:6, nombre:"ryzen-7-7700x", precio:425000}
+    { id: 1, nombre: "ryzen-3-3200g", precio: 99000, img: "https://placehold.co/600x400" },
+    { id: 2, nombre: "ryzen-5-3600", precio: 149000, img: "https://placehold.co/600x400" },
+    { id: 3, nombre: "intelCore-i5-10400F", precio: 136900, img: "https://placehold.co/600x400" },
+    { id: 4, nombre: "intelCore-i7-12700F", precio: 381900, img: "https://placehold.co/600x400" },
+    { id: 5, nombre: "ryzen-7-7700", precio: 379000, img: "https://placehold.co/600x400" },
+    { id: 6, nombre: "ryzen-7-7700x", precio: 425000, img: "https://placehold.co/600x400" }
 ]
 
 const ELEMENTOS_CARRITO = [];
@@ -208,58 +208,67 @@ const CONTENEDOR_PRODUCTOS = document.getElementById('productos-tienda');
 const CONTENEDOR_ELEMENTOS_CARRITO = document.getElementById('elementos-carrito');
 const TOTAL_SPAN = document.getElementById('totalCompra');
 
-function renderProducto(){
-    PRODUCTOS.forEach(producto =>{
+function renderProducto() {
+    PRODUCTOS.forEach(producto => {
         const DIV = document.createElement('div');
         DIV.classList.add('producto');
+
+        /* const IMAGEN = document.createElement('img');
+        IMAGEN.src = producto.img;
+        IMAGEN.alt = producto.nombre;
+        IMAGEN.classList.add('img-producto');
+        CONTENEDOR_PRODUCTOS.appendChild(IMAGEN) */
+
         DIV.innerHTML = `
+        <img src="${producto.img}">
         <h3>${producto.nombre}</h3>
         <p>$${producto.precio}</p>
         <button class="btn btn-primary boton__compra-carrito" id="btn__compra-carrito" data-id="${producto.id}">agregar al carrito</button>      
         `;
         CONTENEDOR_PRODUCTOS.appendChild(DIV);
+        
     })
 }
 
 //AGREGAR PRODUCTOS AL CARRITO
 
-function sumarCarrito(idProducto){
+function sumarCarrito(idProducto) {
     const ITEM_EXISTENTE = ELEMENTOS_CARRITO.find(item => item.id === idProducto);
-    if(ITEM_EXISTENTE){
+    if (ITEM_EXISTENTE) {
         ITEM_EXISTENTE.cantidad++;
-    } else{
+    } else {
         const PRODUCTO = PRODUCTOS.find(p => p.id === idProducto);
-        if(PRODUCTO){
-            ELEMENTOS_CARRITO.push({...PRODUCTO, cantidad:1})
+        if (PRODUCTO) {
+            ELEMENTOS_CARRITO.push({ ...PRODUCTO, cantidad: 1 })
         }
-    } 
+    }
     renderCarrito();
 }
 
 // FUNCION PARA ELIMINAR PRODUCTOS DEL CARRITO
 
-function eliminarDelCarrito(idProducto){
+function eliminarDelCarrito(idProducto) {
     const INDICE = ELEMENTOS_CARRITO.findIndex(item => item.id === idProducto);
-    if(INDICE !== -1){
+    if (INDICE !== -1) {
         ELEMENTOS_CARRITO.splice(INDICE, 1);
         renderCarrito();
     }
 }
 
-function renderCarrito(){
+function renderCarrito() {
     CONTENEDOR_ELEMENTOS_CARRITO.innerHTML = '';
     let precioTotal = 0;
-    ELEMENTOS_CARRITO.forEach(item =>{
+    ELEMENTOS_CARRITO.forEach(item => {
         const LI = document.createElement('li');
         LI.textContent = `${item.nombre} x ${item.cantidad} - $${item.precio * item.cantidad}`
         const BTN_ELIMINAR = document.createElement('button');
         //agregar toastify cuando se eliminar producto metiendo un class o id
         BTN_ELIMINAR.textContent = 'Eliminar';
-        BTN_ELIMINAR.classList.add('btn', 'btn-danger','mx-3', 'my-2')
-        BTN_ELIMINAR.addEventListener('click', ()=>eliminarDelCarrito(item.id));
+        BTN_ELIMINAR.classList.add('btn', 'btn-danger', 'mx-3', 'my-2')
+        BTN_ELIMINAR.addEventListener('click', () => eliminarDelCarrito(item.id));
         LI.appendChild(BTN_ELIMINAR);
         CONTENEDOR_ELEMENTOS_CARRITO.appendChild(LI);
-        precioTotal =+ item.precio * item.cantidad;
+        precioTotal = + item.precio * item.cantidad;
     })
     TOTAL_SPAN.textContent = precioTotal;
 }
@@ -272,7 +281,7 @@ function renderCarrito(){
 
 // USO DE SWEET ALERT . LIBRERIAS
 
-document.getElementById('confirmar-compra').addEventListener('click', ()=>{
+document.getElementById('confirmar-compra').addEventListener('click', () => {
     Swal.fire({
         title: "Quieres confirmar definitivamente?",
         text: "Si la respuesta es si, dale a confirmar",
@@ -282,22 +291,23 @@ document.getElementById('confirmar-compra').addEventListener('click', ()=>{
         cancelButtonColor: "#d33",
         confirmButtonText: "Confirmar!",
         cancelButtonText: "Cancelar!"
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            title: "Perfecto!",
-            text: "Su compra fue realizada!",
-            icon: "success"
-          });
+            Swal.fire({
+                title: "Perfecto!",
+                text: "Su compra fue realizada!",
+                icon: "success"
+            });
             ELEMENTOS_CARRITO.length = 0
             renderCarrito()
         }
-      });
+    });
 });
 
-CONTENEDOR_PRODUCTOS.addEventListener('click', function(evento){
-    if(evento.target.classList.contains('boton__compra-carrito')){
+CONTENEDOR_PRODUCTOS.addEventListener('click', function (evento) {
+    if (evento.target.classList.contains('boton__compra-carrito')) {
         const ID_PRODUCTO = parseInt(evento.target.getAttribute('data-id'));
+        
         sumarCarrito(ID_PRODUCTO);
     }
 })
@@ -308,13 +318,13 @@ renderProducto()
 
 const TOASTIFY = document.getElementById('btn__compra-carrito');
 
-TOASTIFY.addEventListener('click', ()=>{
+TOASTIFY.addEventListener('click', () => {
     Toastify({
         text: "Producto agregado al carrito",
         duration: 2000,
         position: "right",
         gravity: "top",
-        style:{
+        style: {
             background: "green",
         },
     }).showToast()
@@ -335,9 +345,9 @@ console.log(nicolas);
 
 const modo = document.getElementById('modo');
 
-modo.addEventListener("click", ()=>{
+modo.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-    if(document.body.classList.contains("dark")){
+    if (document.body.classList.contains("dark")) {
         localStorage.setItem("modo", "dark");
     } else {
         localStorage.setItem("modo", "claro");
@@ -346,7 +356,7 @@ modo.addEventListener("click", ()=>{
 
 const CAJA_MODO = localStorage.getItem("modo");
 
-if(CAJA_MODO === "dark"){
+if (CAJA_MODO === "dark") {
     document.body.classList.add("dark");
 } else {
     document.body.classList.remove("dark");
@@ -375,11 +385,11 @@ console.log(EJEMPLO_PASADO);
 let cantidadItemsCarrito = 0;
 let carrito = JSON.parse(localStorage.getItem('carrito') || []);
 
-function actualizarIconoCarrito(){
+function actualizarIconoCarrito() {
     const ICONO_CARRITO = document.getElementById('cantidad-items');
-    if(ICONO_CARRITO){
+    if (ICONO_CARRITO) {
         ICONO_CARRITO.textContent = cantidadItemsCarrito;
     }
 }
 
-// minuto 1.18 clase 11
+// minuto 1.18 clase 11 
